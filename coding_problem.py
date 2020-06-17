c874348648558535e1571c775d3a664508247526
rohandhupar <rohandhupar18@gmail.com>
2020-06-17 22:14:32 +0530

    new solution

--- coding_problem.py
+++ coding_problem.py
@@ -88,3 +88,15 @@ class Solution:
             if abs(i-j)>1:
                 return False
         return True
+class Solution:
+    def increasingTriplet(self, nums: List[int]) -> bool:
+        count=0
+        for i in range(1,len(nums)-1):
+            if nums[i]>min(nums[:i]) and nums[i]<max(nums[i+1:]):
+                count+=1
+                
+        if count>=1:
+            return True
+        else :
+            return False
+        
