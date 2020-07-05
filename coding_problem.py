daaa9e6faf97af520cda9b7ae40937426a45dfe1
rohandhupar <rohandhupar18@gmail.com>
2020-07-05 20:05:12 +0530

    new solution

--- coding_problem.py
+++ coding_problem.py
@@ -185,3 +185,19 @@ arr=list(int(i) for i in input().strip().split(' '))
 quickSort(arr, 0,len(arr)-1)
 print(*arr)
 
+def towerofhanoi(n, source, aux, dest):
+    # Please add your code here
+    if n==0:
+        return
+    elif n==1:
+        print(source,dest)
+    else:
+        towerofhanoi(n-1,source,dest,aux)
+        print(source,dest)
+        towerofhanoi(n-1,aux,source,dest)
+        
+        
+        
+
+n=int(input())
+towerofhanoi(n, 'a', 'b', 'c')
