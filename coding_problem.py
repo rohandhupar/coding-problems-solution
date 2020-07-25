adbc437b7c8c07e81d4566732e8098e33c50ed6f
rohandhupar <rohandhupar18@gmail.com>
2020-07-25 20:24:40 +0530

    linked list optimised

--- coding_problem.py
+++ coding_problem.py
@@ -485,4 +485,126 @@ i=int(input())
 l = deleteRec(l, i)
 printll(l)
 
+
+from sys import stdin, setrecursionlimit
+setrecursionlimit(10**5)
+
+#Following is the Node class already written for the Linked List
+class Node :
+    def __init__(self, data) :
+        self.data = data
+        self.next = None
+
+def reversell(head):
+    if head is None:
+        return
+    reversell(head.next)
     
+def lenll(head):
+    count=0
+    while head is not None:
+        head=head.next
+        count+=1
+    return count
+
+def reversell(head1):
+    prev = None
+    current = head1
+    while(current is not None): 
+        next = current.next
+        current.next = prev 
+        prev = current 
+        current = next
+        head1 = prev
+    return head1
+        
+def isPalindrome(head):
+    curr=head
+    head1=head
+    len=lenll(head)
+    if len == 0 or len==1:
+        return True
+    else:
+        if len%2==0:
+            mid=len//2
+            count=0
+            while count<mid:
+                curr=curr.next
+                head1=head1.next
+                count+=1
+            curr.next=None
+            head1=reversell(head1)
+            while head1 is not None and head is not None:
+                if head1.data==head.data:
+                    head1=head1.next
+                    head=head.next
+                    return True
+                else :
+                    return False
+        else:
+            mid=len//2
+            count=0
+            while count<mid:
+                curr=curr.next
+                head1=head1.next
+                count+=1
+            head1=head1.next
+            curr.next=None
+            head1=reversell(head1)
+            while head1 is not None and head is not None:
+                if head1.data==head.data:
+                    head1=head1.next
+                    head=head.next
+                    return True
+                else :
+                    return False
+            
+
+#Taking Input Using Fast I/O
+def takeInput() :
+    head = None
+    tail = None
+
+    datas = list(map(int, stdin.readline().rstrip().split(" ")))
+
+    i = 0
+    while (i < len(datas)) and (datas[i] != -1) :
+        data = datas[i]
+        newNode = Node(data)
+
+        if head is None :
+            head = newNode
+            tail = newNode
+
+        else :
+            tail.next = newNode
+            tail = newNode
+
+        i += 1
+
+    return head
+
+
+#to print the linked list 
+def printLinkedList(head) :
+
+    while head is not None :
+        print(head.data, end = " ")
+        head = head.next
+
+    print()
+
+
+#main
+t = int(stdin.readline().rstrip())
+
+while t > 0 :
+    
+    head = takeInput()
+    
+    if isPalindrome(head) :
+        print('true')
+    else :
+        print('false')
+        
+    t -= 1   
